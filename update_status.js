ea8a8aa32906f9756614bed45d8191e614be50bb
Supra Admin <admin@supraphutho.local>
2026-06-24 15:39:25 +0700

    Auto update data

--- update_status.js
+++ update_status.js
@@ -1,1 +1,1 @@
-﻿window.UPDATE_STATUS = {"status":"running","timestamp":"2026-06-24 15:39:21","message":"","step":"Dang dong bo du lieu len link online...","progress":95};
+﻿window.UPDATE_STATUS = {"status":"success","timestamp":"2026-06-24 15:39:25","message":"","step":"Hoan thanh cap nhat!","progress":100};
