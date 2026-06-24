5c5074fc2274bba00d4edf143c2c3747ecc7b97b
Supra Admin <admin@supraphutho.local>
2026-06-24 15:42:52 +0700

    Auto update data

--- update_status.js
+++ update_status.js
@@ -1,1 +1,1 @@
-﻿window.UPDATE_STATUS = {"status":"error","timestamp":"2026-06-24 15:42:37","message":"Mot tien trinh cap nhat khac (PID 6064) dang chay. Vui long doi.","step":"Loi trung lap tien trinh","progress":0};
+﻿window.UPDATE_STATUS = {"status":"error","timestamp":"2026-06-24 15:42:51","message":"Mot tien trinh cap nhat khac (PID 6064) dang chay. Vui long doi.","step":"Loi trung lap tien trinh","progress":0};
