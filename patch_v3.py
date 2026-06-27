3134798addf1e254bd10a97e9386892d5688a514
Supra Admin <admin@supraphutho.local>
2026-06-27 22:39:54 +0700

    Clean up temporary patch and helper files

--- patch_v3.py
+++
@@ -1,7 +0,0 @@
-with open('demo.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-html = html.replace('src="trips_logic_v5.js?v=2"', 'src="trips_logic_v5.js?v=3"')
-
-with open('demo.html', 'w', encoding='utf-8') as f:
-    f.write(html)
--- patch_v4.py
+++
@@ -1,7 +0,0 @@
-with open('demo.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-html = html.replace('src="trips_logic_v5.js?v=3"', 'src="trips_logic_v5.js?v=4"')
-
-with open('demo.html', 'w', encoding='utf-8') as f:
-    f.write(html)
