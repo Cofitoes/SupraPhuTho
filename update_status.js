1dfce5e2fa81942e89e48f30dc60e2d4beb27761
Supra Admin <admin@supraphutho.local>
2026-06-29 01:38:44 +0700

    Auto-update booking data and system files from dashboard

--- update_status.js
+++ update_status.js
@@ -1,1 +1,1 @@
-﻿window.UPDATE_STATUS = {"status":"running","timestamp":"2026-06-29 01:37:35","message":"","step":"Dang dong bo du lieu len link online...","progress":95};
+﻿window.UPDATE_STATUS = {"status":"running","timestamp":"2026-06-29 01:38:43","message":"","step":"Dang dong bo du lieu len link online...","progress":95};
